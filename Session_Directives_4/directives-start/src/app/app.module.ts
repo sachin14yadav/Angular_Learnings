9f9b1ae4014d051dc7b5c64739deb233eba2d6d7
Sachin Yadav <45989793+sachin14yadav@users.noreply.github.com>
2018-12-20 21:11:17 +0530

    Adding new project for Directives

---
+++ Session_Directives_4/directives-start/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicHighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
